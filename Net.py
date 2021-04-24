c5c3ebff4fbff3231a61aa653b554f6b7fb14e95
Johnathan Joseph Toloff <jtoloff@umich.edu>
2021-04-24 18:59:48 -0400

    LeNet-5 NN Architecture w/ extra Linear layer

--- Net.py
+++ Net.py
@@ -9,31 +9,30 @@ class Net(nn.Module):
     super(Net, self).__init__()
     self.n_class = N_CLASS
     self.layers = nn.Sequential(
-        # Convolutional Layers
-        nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, padding=1),
+        # LeNet-5 Layers
+        nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, padding=2),
+        nn.ReLU(inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2),
-        nn.Conv2d(in_channels=8, out_channels=64, kernel_size=3, padding=1),
+        nn.Conv2d(in_channels=8, out_channels=64, kernel_size=5, padding=2),
+        nn.ReLU(inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2),
         nn.Flatten(),
-        # Fully connected layers w dropout
         nn.Linear(in_features=16384, out_features=2048),
         nn.ReLU(inplace=True),
-        nn.Dropout(p=DROPOUT_PROB),
         nn.Linear(in_features=2048, out_features=512),
         nn.ReLU(inplace=True),
-        nn.Dropout(p=DROPOUT_PROB),
         nn.Linear(in_features=512, out_features=N_CLASS)
         )
 
   def forward(self, x):
     return self.layers(x)
-    
-    
+
+
 def printModel():
     # net = Net().to(device)
     net = Net()
     # visualizing the model
     print('Your network:')
     summary(net, (3, IMAGE_DIM, IMAGE_DIM))
-    
+
     return net
